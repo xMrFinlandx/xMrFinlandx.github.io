dce843abd8fed32d0f57120c2df9ddb8c1660e74
Ivan Frolov <xMrFinlandx@gmail.com>
2023-10-01 15:32:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", () => {
 
 let usercard = document.getElementById("usercard")
 let p = document.createElement("p")
-p.innerText = `${tg.initDataUnsafe.user.first_name}`;
+
+p.innerText = `${tg.initDataUnsafe.user.first_name} is your name`;
 
 usercard.appendChild(p);
